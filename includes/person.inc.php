453f1896bfc1ad234eb7f7ab76c3edb81afb99b9
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2024-09-11 10:51:22 +0200

    my first commit

---
+++ includes/person.inc.php
@@ -0,0 +1,13 @@
+<?php
+class Person {
+    private $first = "Daniel";
+    private $last = "Nielsen";
+    private $age = "28";
+}
+
+class Pet {
+    private function owner() {
+        $a = "Hi there";
+        return $a;
+    }
+}
